967439ecad7a1e04ecb903eb418a7c7fdbdbfc77
vitorsala <vitorsala@gmail.com>
2016-07-12 17:46:47 -0300

    projeto.

---
+++ Assets/Scripts/HorizontalCosMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalCosMovement : MonoBehaviour {
+	
+	public float magnitude;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.right * Mathf.Cos(Time.timeSinceLevelLoad) * magnitude);
+	}
+}
---
+++ Assets/Scripts/VerticalSinMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalSinMovement : MonoBehaviour {
+
+	public float magnitude;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector2.up * Mathf.Sin(Time.timeSinceLevelLoad) * magnitude);
+	}
+}
